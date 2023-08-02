9f67c060b487ebd917596fb155b62104805196ef
AngeloDima <angelodimauro91@gmail.com>
2023-08-02 12:53:33 +0200

    Sistemazione della HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Sec1BannerComponent } from './home-page/sec1-banner/sec1-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    Sec1BannerComponent
   ],
   imports: [
     BrowserModule,
