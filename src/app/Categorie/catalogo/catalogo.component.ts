33db101c5002ee189b574cfa4d83bd6106719e8b
AngeloDima <angelodimauro91@gmail.com>
2023-08-10 12:40:05 +0200

    start Pagina CATALOGO

--- src/app/Categorie/catalogo/catalogo.component.ts
+++ src/app/Categorie/catalogo/catalogo.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtenteService } from 'src/app/CentroDati/utente.service';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.css']
 })
-export class CatalogoComponent {
+export class CatalogoComponent implements OnInit {
+
+
+  utenteVerificato: any;
+  toggle: boolean = false;
+
+  constructor(private ute: UtenteService) { }
+
+  ngOnInit(): void {
+    this.utenteVerificato = this.ute.utenteLoggato;
+  }
+
+  isToggle() {
+    this.toggle = !this.toggle;
+  }
+
+  closeMenu() {
+    this.toggle = false;
+  }
 
 }
