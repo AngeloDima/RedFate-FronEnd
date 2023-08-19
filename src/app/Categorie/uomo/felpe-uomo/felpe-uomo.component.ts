e53959ee8bcc7d55c66619bb5f2284ffaeda9f18
AngeloDima <angelodimauro91@gmail.com>
2023-08-20 00:16:54 +0200

    animazione in entrata e in uscita dalla rotta SOLO PER FELPE

--- src/app/Categorie/uomo/felpe-uomo/felpe-uomo.component.ts
+++ src/app/Categorie/uomo/felpe-uomo/felpe-uomo.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-felpe-uomo',
   templateUrl: './felpe-uomo.component.html',
   styleUrls: ['./felpe-uomo.component.css'],
   animations: [
-    trigger('prova', [
-      state('void', style({ opacity: 0 })),
-      state('*', style({ opacity: 1 })),
-      transition(':enter', animate('600ms ease-in')),
-      transition(':leave', animate('600ms ease-out')),
+    trigger('fadeInOut', [
+      state('visible', style({ opacity: 1, transform: 'scale(1)' })),
+      transition('void => visible', [
+        style({ opacity: 0, transform: 'scale(0)' }),
+        animate('400ms ease-in'),
+      ]),
+      transition('visible => void', [
+        animate('400ms ease-out', style({ opacity: 0, transform: 'scale(0)' })),
+      ]),
     ]),
   ],
 })
 export class FelpeUOMOComponent {
+  animationState: string = 'visible';
 
+  constructor(private router: Router) { }
+
+  leavePage() {
+    this.animationState = 'void';
+    setTimeout(() => {
+      // After the exit animation is complete, navigate to the new route
+      this.router.navigate(['/Uomo']);
+    }, 400);
+  }
 }
-
-
-
