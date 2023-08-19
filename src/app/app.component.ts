cd971171065192f3b3cfd77773bd30ba97926722
AngeloDima <angelodimauro91@gmail.com>
2023-08-19 17:23:29 +0200

    nascondo HEADER e FOOTER nel login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtenteService } from './CentroDati/utente.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StoreFullStack';
+
+  constructor(private router: Router) { }
+
+  prova(): boolean {
+    const currentUrl = this.router.url;
+
+
+    if (currentUrl === '/login') {
+      return false; // Nascondi header e footer
+    } else {
+      return true; // Mostra header e footer
+    }
+  }
 }
