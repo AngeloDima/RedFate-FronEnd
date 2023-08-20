5fcaa4aeee57913bca6e17b5c6b2ca9e9338a568
AngeloDima <angelodimauro91@gmail.com>
2023-08-20 19:53:26 +0200

    replico tutte le logiche anche nella categorie e sotto categorie DONNA

--- src/app/Categorie/donna/felpe-donna/felpe-donna.component.ts
+++ src/app/Categorie/donna/felpe-donna/felpe-donna.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-felpe-donna',
   templateUrl: './felpe-donna.component.html',
-  styleUrls: ['./felpe-donna.component.css']
+  styleUrls: ['./felpe-donna.component.css'],
+  animations: [
+    trigger('fadeOutUp', [
+      state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('void => visible', [
+        style({ opacity: 0, transform: 'translateY(-100%)' }),
+        animate('400ms ease-in'),
+      ]),
+      transition('visible => void', [
+        animate('400ms ease-out', style({ opacity: 0, transform: 'translateY(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class FelpeDonnaComponent {
+  animationState: string = 'visible';
 
+  constructor(private router: Router) { }
+
+  leavePage() {
+    this.animationState = 'void';
+    setTimeout(() => {
+      // After the exit animation is complete, navigate to the new route
+      this.router.navigate(['/Donna']);
+    }, 300);
+  }
 }
--- src/app/Categorie/donna/pantaloni-donna/pantaloni-donna.component.ts
+++ src/app/Categorie/donna/pantaloni-donna/pantaloni-donna.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pantaloni-donna',
   templateUrl: './pantaloni-donna.component.html',
-  styleUrls: ['./pantaloni-donna.component.css']
+  styleUrls: ['./pantaloni-donna.component.css'],
+  animations: [
+    trigger('fadeOutUp', [
+      state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('void => visible', [
+        style({ opacity: 0, transform: 'translateY(-100%)' }),
+        animate('400ms ease-in'),
+      ]),
+      transition('visible => void', [
+        animate('400ms ease-out', style({ opacity: 0, transform: 'translateY(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class PantaloniDonnaComponent {
+
+
+  animationState: string = 'visible';
+
+  constructor(private router: Router) { }
+
+  leavePage() {
+    this.animationState = 'void';
+    setTimeout(() => {
+      // After the exit animation is complete, navigate to the new route
+      this.router.navigate(['/Donna']);
+    }, 300);
+  }
+
 
 }
